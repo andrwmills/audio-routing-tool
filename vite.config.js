f0cc6488fc57e6cc402d12529a11cda1f3554207
andrwmills <andrwmillss@gmail.com>
2025-02-12 09:09:18 +0000

    Added Vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",  // 👈 Ensure Vite knows where the root folder is
+  build: {
+    outDir: "dist",
+  },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
+import react from '@vitejs/plugin-react';
